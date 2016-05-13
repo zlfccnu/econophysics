dd250832e7eac8c886e29fd77c6e0e34a600263f
zlfccnu <zlfccnu@foxmail.com>
2016-05-13 12:11:33 +0800

    function to initialize parameters

--- R/initializePars.R
+++ R/initializePars.R
@@ -1,5 +1,7 @@
-#'Function used to initialize some system parameters
-#'@export
+#' Function used to initialize some system parameters
+#' @return can not be seen 
+#' @export
+
 initializePars=function(){
   Sys.setlocale("LC_TIME", "en_US.UTF-8")
   options(scipen=100,max.print=2000,editor = 'nano')
