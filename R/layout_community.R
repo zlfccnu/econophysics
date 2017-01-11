98da459669224194325eda822c900170221df0f7
zlfccnu <zlfccnu@foxmail.com>
2017-01-11 14:24:47 -0500

    modify the layout community function

--- R/layout_community.R
+++ R/layout_community.R
@@ -4,19 +4,24 @@
 #'@param layout A character name for the layout function you use
 #'@export
 #'@return A data frame with four columns, the vertex label, the membership of each node, two columns of the coordinates
-layout_community<- function(graph,algorithm_name,layout){
+layout_community<- function(graph,community_algorithm,layout_desired){
   if (!is_igraph(graph)) {
     stop("Not a graph object")
   }
-  community_algorithm<- match.fun(algorithm_name)
+  ## detect the community
   graph_commmunity<- community_algorithm(graph)
+  ## get the membership
   graph_membership<- membership(graph_commmunity)
+  ## get the vertex label in each membership
   graph_vertex_list<- lapply(1:max(graph_membership),function(x){which(graph_membership==x)})
+  ## split the network according to the community structure
   split_community<- lapply(graph_vertex_list,function(x){induced_subgraph(graph,x)})## the vertex name will be renamed
-  layout_func<- match.fun(layout)
-  layouts<- lapply(split_community,layout_func)
+  ## get the layout for each sub network
+  layouts<- lapply(split_community,layout_desired)
+  ## combine the layout all the sub network
   lay<- merge_coords(split_community,layouts)
   
+  ## the final outputs
   lay<- cbind(unlist(graph_vertex_list),sort(graph_membership),lay)
   colnames(lay)<- c("vertex_label","community_mem","coord1","coord2")
   lay<- lay[order(lay[,1]),]
