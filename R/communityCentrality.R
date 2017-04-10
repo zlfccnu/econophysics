85dabea78515f944804ecfdee54260e51b7f0994
zlfccnu <zlfccnu@foxmail.com>
2017-04-10 17:30:25 -0400

    modularity matrix

---
+++ R/communityCentrality.R
@@ -0,0 +1,14 @@
+#' community centrality of network
+#' @param graph an igraph object
+#' @param diag set the diag elements of null model matrix to zero
+#' @return the modularity matrix
+#' @export
+
+communityCentrality=function(graph,diag=FALSE){
+  modMat=modularityMatrix(graph = graph,diag=diag)
+  modMatEigen=eigen(modMat)
+  p=length(which(modMatEigen$values>0))
+  x=sqrt(modMatEigen$values[1:p])*modMatEigen$vectors[1:p,]
+  y=apply(x,MARGIN = 2,function(x){sqrt(sum(x^2))})
+  return(y)
+}
--- R/modularityMatrix.R
+++ R/modularityMatrix.R
@@ -1,16 +1,18 @@
-#' Function used to filter the covariance matrix of a series of return time series
+#' modularity matrix for netowrk
 #' @param graph an igraph object
 #' @return the modularity matrix
 #' @export
 
-modularityMatrix=function(graph){
+modularityMatrix=function(graph,diag=TRUE){
   if(!is.igraph(graph = graph)){
     stop("must be an igraph object")
   }
-  adjMat=as.matrix(get.adjacency(graph = graph))
+  adjMat=as.matrix(get.adjacency(graph = graph,attr = "weight"))
   strengthSeq=strength(graph = graph)
-  nullModelMatrix=strengthSeq%*%t(strengthSeq)/(0.5*sum(strengthSeq))
-  diag(nullModelMatrix)=0
+  nullModelMatrix=strengthSeq%*%t(strengthSeq)/(sum(strengthSeq))
+  if(diag==TRUE){
+    diag(nullModelMatrix)=0
+  }
   modMat=adjMat - nullModelMatrix
   return(modMat)
   }
