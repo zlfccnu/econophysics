f482d59ab37c1bb2d3d543b6bb92d04f20e13c84
zlfccnu <zlfccnu@foxmail.com>
2017-11-23 11:48:37 -0500

    safe save

--- R/safeSave.R
+++ R/safeSave.R
@@ -1,6 +1,11 @@
+#'safe save for objects
+#'@param ... the objects
+#'@param file the file name to store the objects
+#'@param overwrite overwrite the existed file or not
+#'@param the function used to save the objects 
+#'@export
 
-
-safeSave <- function( ..., file=stop("'file' must be specified"), overwrite=FALSE, save.fun=save) {
+safeSave <- function( ..., file=stop("'file' must be specified"), overwrite=FALSE, save.fun=save.image) {
   if ( file.exists(file) & !overwrite ) stop("'file' already exists")
   save.fun(..., file=file)
 }
