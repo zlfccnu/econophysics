d83bf2fbb585432915ba157274a69dbb0207cc79
zlfccnu <zlfccnu@foxmail.com>
2018-01-19 15:18:56 -0500

    select stocks

--- R/selectPortfoiloInsideCommunity.r
+++ R/selectPortfoiloInsideCommunity.r
@@ -3,6 +3,7 @@
 #' @param mem the community membership of te GRAPH
 #' @param stock_id the names of the stocks 
 #' @param portfolioSize the size of the portfolio
+#' @return a vector with stock names
 selectPortfolioInsideCommunity=function(GRAPH,mem,stock_id,portfolioSize=4){
   communitySize=sizes(mem)
   mem_id=which(communitySize>=portfolioSize)
