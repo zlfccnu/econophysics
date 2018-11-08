4e1414b9a805fc7bff2a744d6c4c61aef7bfa573
zlfccnu <zlfccnu@foxmail.com>
2018-11-08 19:56:33 +0800

    citation2net,correctnames

---
+++ R/citation2net.r
@@ -0,0 +1,36 @@
+#' convert the citation information to co-occurrence network
+#' @param database A dataframe with authors, institutions, keywords at each row
+#' @return an weighted and named igraph object
+citation2net=function(database){
+  require(igraph)
+  ## construct the key words network
+  database=as.data.frame(database)
+  database %>% unlist() %>% unique() %>% na.omit() %>% as.character()-> databaseList
+  databaseNet= graph.empty(directed = FALSE)
+  databaseNet=databaseNet + vertices(databaseList)
+  
+  for(i in 1:dim(database)[1]){
+    eachRow=as.character(na.omit(unlist(database[i,])))
+    if(length(eachRow)>=2){
+      eachRow=combn(eachRow,2)
+      databaseNet=add.edges(graph = databaseNet,edges = as.vector(eachRow))
+    }
+    print(i)
+  }
+  ## simplify the network, delete duplicate edges etc.
+  databaseNet=simplify(databaseNet)
+  
+  ## add edge weight
+  databaseNet=set.edge.attribute(databaseNet,"weight",value=0)
+  for(i in 1:dim(database)[1]){
+    eachRow=as.character(na.omit(unlist(database[i,])))
+    if(length(eachRow)>=2){
+      eachRow=combn(eachRow,2)
+      for(j in 1:dim(eachRow)[2]){
+        E(databaseNet)[eachRow[1,j]%--%eachRow[2,j]]$weight=E(databaseNet)[eachRow[1,j]%--%eachRow[2,j]]$weight+1
+      }
+    }
+    print(i)
+  }
+  return(databaseNet)
+}
---
+++ R/correctNames.r
@@ -0,0 +1,15 @@
+#` This function is aiming at converting author names to standard format: family name, Short. Short.(Zhu, Z. W.;Keller, A. Z.), remove blank space
+#' @param x a string represents a name or a vector
+#' @return a string 
+correctNames=function(x){
+  tmp=function(x){
+    if(is.na(x)!=TRUE){
+      x=gsub('[[:punct:] ]+',' ',x)
+      x=strsplit(x,split = " ")[[1]]
+      x=paste(paste0(x[1],",",collapse = ""),paste(paste0(x[-1],"."),collapse = " "))
+    }
+    return(x)
+  }
+  x=sapply(x,tmp)
+  return(x)
+}
